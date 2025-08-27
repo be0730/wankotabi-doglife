4b001ec68c69f4ee191e6fb42dfd72a80925d1ad
be0730 <wkkey0730@gmail.com>
2025-08-27 11:10:32 +0900

    facilitiesテーブルDB作成

--- db/migrate/20250826085706_create_facilities.rb
+++ db/migrate/20250826085706_create_facilities.rb
@@ -1,9 +1,33 @@
 class CreateFacilities < ActiveRecord::Migration[8.0]
   def change
     create_table :facilities do |t|
-      t.string :title
+      t.references :user, null: false, foreign_key: true
 
-      t.timestamps
+      t.string :title, null: false
+      t.integer :category, null: false, default: 0
+      t.text :overview
+      t.string :full_address
+      t.string :postal_code
+      t.string :prefecture_code
+      t.string :prefecture_name
+      t.string :city
+      t.string :street
+      t.string :building
+      t.decimal :latitude
+      t.decimal :longitude
+      t.string :phone_number
+      t.string :business_hours
+      t.string :closed_day
+      t.string :homepage_url
+      t.string :instagram_url
+      t.string :facebook_url
+      t.string :X_url
+      t.string :supplement
+
+      t.timestamps null: false
     end
+    add_index :facilities, :category
+    add_index :facilities, :prefecture_code
+    add_index :facilities, [:latitude, :longitude]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_25_082628) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_26_085706) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "facilities", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.integer "category", default: 0, null: false
+    t.text "overview"
+    t.string "full_address"
+    t.string "postal_code"
+    t.string "prefecture_code"
+    t.string "prefecture_name"
+    t.string "city"
+    t.string "street"
+    t.string "building"
+    t.decimal "latitude"
+    t.decimal "longitude"
+    t.string "phone_number"
+    t.string "business_hours"
+    t.string "closed_day"
+    t.string "homepage_url"
+    t.string "instagram_url"
+    t.string "facebook_url"
+    t.string "X_url"
+    t.string "supplement"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category"], name: "index_facilities_on_category"
+    t.index ["latitude", "longitude"], name: "index_facilities_on_latitude_and_longitude"
+    t.index ["prefecture_code"], name: "index_facilities_on_prefecture_code"
+    t.index ["user_id"], name: "index_facilities_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +56,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_25_082628) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "facilities", "users"
 end
