b8da8bb7c31d5481ff5bbb1009644d27515fccc2
be0730 <wkkey0730@gmail.com>
2025-08-29 16:05:43 +0900

    facilities_controllerへpermit内容追加

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -57,8 +57,10 @@ class FacilitiesController < ApplicationController
 
   def facility_params
     params.require(:facility).permit(
-      :title, :category, :postal_code, :prefecture_code, :prefecture_name,
-      :city, :street, :building, :latitude, :longitude, :overview
+      :title, :category, :postal_code, :prefecture_name,
+      :city, :street, :building, :latitude, :longitude,
+      :overview, :phone_number, :business_hours, :closed_day,
+      :homepage_url, :instagram_url, :facebook_url, :x_url, :supplement
     )
   end
 end
