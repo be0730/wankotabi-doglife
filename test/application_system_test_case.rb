afe765dd03e654d815cd9997644e2cd6a8170c32
be0730 <wkkey0730@gmail.com>
2025-09-04 17:55:46 +0900

    test修正

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,9 @@
 require "test_helper"
+require "selenium/webdriver"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  include Warden::Test::Helpers
+  setup   { Warden.test_mode! }
+  teardown{ Warden.test_reset! }
 end
