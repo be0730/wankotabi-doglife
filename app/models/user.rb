649d3efaa51d422d7efb0fcf2958ec7de861c16e
be0730 <wkkey0730@gmail.com>
2025-09-09 20:05:12 +0900

    userDBへavatarカラム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_facilities, through: :favorites, source: :facility
 
+  mount_uploader :avatar, AvatarUploader
+
   def favorite(facility)
     favorites.find_or_create_by!(facility: facility)
   end
