05e8cdbb0c422034210cbdc2229d108ded779e82
be0730 <wkkey0730@gmail.com>
2025-09-10 20:15:00 +0900

    プロフィール画像修正機能作成、フラッシュメッセージ修正

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -39,7 +39,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
-    %w(jpg jpeg gif png)
+    %w[jpg jpeg gif png webp]
   end
 
   def size_range
