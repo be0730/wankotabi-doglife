109c0d300f31013f449b3796b188f611d2a2af7a
be0730 <wkkey0730@gmail.com>
2025-09-17 15:56:52 +0900

    プロフィール画像をヘッダーに表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def avatar_src_for(user, size: 80)
+    if user.respond_to?(:avatar) && user.avatar.present?
+      url = user.avatar.url
+      # キャッシュバスト（同名上書き対策）
+      url = "#{url}?v=#{user.updated_at.to_i}" if user.respond_to?(:updated_at)
+      url
+    end
+  end
+
+  def avatar_image_tag(user, size: 40, **opts)
+    src = avatar_src_for(user, size: size)
+    classes = [ "rounded-full", opts.delete(:class) ].compact.join(" ")
+    image_tag(src, width: size, height: size, class: classes, alt: user&.name, **opts)
+  end
 end
