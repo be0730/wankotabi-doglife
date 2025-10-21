b88d1c87a826d726b350c807b228738566cec3c8
be0730 <wkkey0730@gmail.com>
2025-10-21 11:27:31 +0900

    header修正（メニュー周り部分テンプレ化）

--- app/helpers/facilities_helper.rb
+++ app/helpers/facilities_helper.rb
@@ -6,4 +6,14 @@ module FacilitiesHelper
       homepage_url instagram_url facebook_url x_url supplement
     ]
   end
+
+  def category_badge_classes(category)
+    map = {
+      "accommodation"  => "bg-green-100 text-green-800",
+      "restaurant"  => "bg-amber-100 text-amber-800",
+      "leisure" => "bg-blue-100 text-blue-800",
+      "shop"  => "bg-purple-100 text-purple-800",
+    }
+    map.fetch(category.to_s, "bg-gray-100 text-gray-700")
+  end
 end
