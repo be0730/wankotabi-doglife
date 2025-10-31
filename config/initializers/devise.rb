85d138733fdf7baec30eca84b0dcea2d0f44bf56
be0730 <wkkey0730@gmail.com>
2025-10-31 17:33:18 +0900

    omniauth認証追加

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,8 +273,10 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2,
-  Rails.application.credentials.dig(:google, :client_id),
-  Rails.application.credentials.dig(:google, :client_secret)
+    Rails.application.credentials.dig(:google, :client_id),
+    Rails.application.credentials.dig(:google, :client_secret),
+    scope: 'email,profile',
+    prompt: 'select_account'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users_omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :users, only: [ :show ] do
     member { get :post_list }
