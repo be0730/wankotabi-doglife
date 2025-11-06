40c7fe7cb77664fdef4b5db3c257cffa4af2bd28
be0730 <wkkey0730@gmail.com>
2025-11-06 11:23:39 +0900

    rails g model facilities_tags

---
+++ db/migrate/20251106021227_create_facilities_tags.rb
@@ -0,0 +1,9 @@
+class CreateFacilitiesTags < ActiveRecord::Migration[8.0]
+  def change
+    create_table :facilities_tags do |t|
+      t.references :facility, null: false, foreign_key: { on_delete: :cascade }
+      t.references :tag,      null: false, foreign_key: { on_delete: :cascade }
+    end
+    add_index :facilities_tags, [:facility_id, :tag_id], unique: true
+  end
+end
