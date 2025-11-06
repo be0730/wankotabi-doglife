09398131dbb37248cae5ef422c6d8a011f70931f
be0730 <wkkey0730@gmail.com>
2025-11-06 11:09:36 +0900

    tag db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_31_024638) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_06_020607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -100,6 +100,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_31_024638) do
     t.integer "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_tags_on_key", unique: true
   end
 
   create_table "users", force: :cascade do |t|
