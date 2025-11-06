3cac38ee28faafd1e4b3260ea791c336bd18ae20
be0730 <wkkey0730@gmail.com>
2025-11-06 11:57:48 +0900

    tagモデル追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,12 @@
 class Tag < ApplicationRecord
+  has_many :facilities_tags, dependent: :destroy
+  has_many :facilities, through: :facilities_tags
+
+  validates :key,  presence: true, uniqueness: true
+  validates :name, presence: true
+
+  # アセット画像のパス（例: app/assets/images/tags/"ファイル名".svg）
+  def icon_asset_path
+    "tags/#{key}.svg"
+  end
 end
