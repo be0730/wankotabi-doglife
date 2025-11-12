2432d2e2f776492a497eeaaa52a8828cd464ffa2
be0730 <wkkey0730@gmail.com>
2025-11-12 16:46:37 +0900

    OGP出力用のヘルパー追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,35 @@ module ApplicationHelper
 
     image_tag(src, **options)
   end
+
+  def default_meta_tags
+    {
+      site:        "WankoTabi DogLife",
+      title:       "わんこたび",
+      reverse:     true,
+      separator:   "|",
+      description: "犬と一緒に行ける施設を検索・共有できるサービスです。",
+      keywords:    %w[犬 犬連れ 旅行 お出かけ ドッグラン カフェ ホテル 施設 検索 共有],
+      canonical:   request.original_url,
+      noindex:     ! Rails.env.production?,
+      icon:[
+        { href: image_url("favicon.ico") },
+        { href: image_url("icon.png"), rel: "apple-touch-icon", sizes: "192x192", type: "image/png" },
+      ],
+      og: {
+        site_name:   :site,
+        title:       :title,
+        type:        "website",
+        url:         :canonical,
+        description: :description,
+        image:       image_url("ogp.png"),
+        locale:      "ja_JP",
+      },
+      twitter: {
+        card: "summary_large_image",
+        site: "@",
+        image: image_url("ogp.png")
+      }
+    }
+  end
 end
