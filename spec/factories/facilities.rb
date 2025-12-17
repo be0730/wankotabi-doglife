b393568d970b9cd7cba52857d8ae46e487d417c7
be0730 <wkkey0730@gmail.com>
2025-12-17 21:30:32 +0900

    factories/facilities.rbへ記載追加

--- spec/factories/facilities.rb
+++ spec/factories/facilities.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
-  factory :task do
-    
+  factory :facility do
+    sequence(:title) { |n| "施設名_#{n}" }
+    category { 1 }
+    city { "新宿区" }
+    street { "西新宿1-1" }
+
+    association :user
+    association :prefecture
   end
 end
