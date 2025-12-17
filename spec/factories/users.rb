aa6da5d6e2158a5ad44dd16f15b42be6a8739b84
be0730 <wkkey0730@gmail.com>
2025-12-17 21:31:00 +0900

    user_spec修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:name) { |n| "user_#{n}_#{rand(1000)}" }
+    sequence(:email) { |n| "user_#{n}_#{rand(1000)}@example.com" } # 乱数を追加
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,23 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'バリデーションチェック' do
+    let(:user) { build(:user) }
+
     it "nameが空欄の場合、バリデーションエラーになるか" do
-      user = User.new(name: "")
-      user.valid?
+      user.name = ""
+      expect(user).to be_invalid
       expect(user.errors[:name]).to include("を入力してください")
     end
     it "nameが21文字以上の場合、バリデーションエラーになるか" do
-      user = User.new(name: "a" * 21)
-      user.valid?
+      user.name = "a" * 21
+      expect(user).to be_invalid
       expect(user.errors[:name]).to include("は20文字以内で入力してください")
     end
     it "nameが重複している場合、バリデーションエラーになるか" do
-      User.create!(name: "testuser", email: "test@example.com", password: "password")
-      user = User.new(name: "testuser", email: "test2@example.com", password: "password")
-      user.valid?
-      expect(user.errors[:name]).to include("はすでに存在します")
+      create(:user, name: "testuser")
+      another_user = build(:user, name: "testuser")
+      expect(another_user).to be_invalid
+      expect(another_user.errors[:name]).to include("はすでに存在します")
     end
   end
 end
