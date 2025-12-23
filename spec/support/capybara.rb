7bedbb3259dc9c26ab0bc6af71808ae2b683e43a
be0730 <wkkey0730@gmail.com>
2025-12-23 10:14:49 +0900

    システムスペック設定用ファイル編集

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,8 +1,30 @@
+require "capybara/rspec"
+require "selenium/webdriver"
+
 Capybara.register_driver :remote_chrome do |app|
+  url = ENV.fetch("SELENIUM_REMOTE_URL", "http://selenium:4444/wd/hub")
+
   options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('no-sandbox')
-  options.add_argument('headless')
-  options.add_argument('disable-gpu')
-  options.add_argument('window-size=1680,1050')
-  Capybara::Selenium::Driver.new(app, browser: :remote, url: ENV['SELENIUM_DRIVER_URL'], capabilities: options)
+  %w[headless no-sandbox disable-dev-shm-usage].each { |arg| options.add_argument(arg) }
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    url: url,
+    capabilities: options
+  )
+end
+
+Capybara.server_host = "0.0.0.0"
+Capybara.server_port = 3001
+Capybara.app_host = "http://web:#{Capybara.server_port}"
+
+RSpec.configure do |config|
+  config.before(:each, type: :system, js: true) do
+    driven_by :remote_chrome
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :remote_chrome
+  end
 end
